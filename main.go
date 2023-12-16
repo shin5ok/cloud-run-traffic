0b5a6df27c54f7fa13867270f0c135d0aaf78854
shin5ok <shin5ok@55mp.com>
2023-12-17 01:15:57 +0900

    change the logging driver to slog

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"time"
@@ -11,7 +11,10 @@ import (
 
 var (
 	version       = os.Getenv("VERSION")
+	cloudRunRev   = os.Getenv("K_REVISION")
+	port          = os.Getenv("PORT")
 	versionString string
+	logger        *slog.Logger
 )
 
 func init() {
@@ -21,11 +24,44 @@ func init() {
 	versionString = "version: " + version
 }
 
+func init() {
+
+	replace := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.LevelKey && a.Value.String() == slog.LevelWarn.String() {
+			return slog.String("severity", "WARNING")
+		}
+		if a.Key == "level" {
+			return slog.String("severity", a.Value.String())
+		}
+		if a.Key == "msg" {
+			return slog.String("message", a.Value.String())
+		}
+		return a
+	}
+
+	options := slog.HandlerOptions{
+		Level:     slog.LevelInfo,
+		AddSource: true, ReplaceAttr: replace,
+	}
+
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, &options))
+	slog.SetDefault(logger)
+
+}
+
 func main() {
 	r := chi.NewRouter()
+
 	r.Get("/version", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(versionString + "\n"))
-		log.Println(versionString)
+		logger.Info("api version",
+			"version", versionString,
+			"revision", cloudRunRev,
+		)
 	})
-	http.ListenAndServe(":8080", r)
+
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, r)
 }
